0ce0ae9c2f1bb3705743248256b072bae8ed2f21
Marc F. Mezger <marc.mezger@gmail.com>
2023-08-20 15:00:32 +0200

    Adding first idea.

--- asr_summary_llm/main.py
+++ asr_summary_llm/main.py
@@ -1,28 +1,21 @@
 import whisper
+import torch
 from pyannote.audio import Pipeline
 
-pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization')
-
-DEMO_FILE = {'uri': 'blabal', 'audio': 'audio.wav'}
-dz = pipeline(DEMO_FILE)  
-
-with open("diarization.txt", "w") as text_file:
-    text_file.write(str(dz))
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
 def main():
-    pass
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization@2.1",
+        use_auth_token="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",
+    )
+    pipeline = pipeline.to(torch.device(0))
+    DEMO_FILE = {"uri": "blabal", "audio": "audio.wav"}
+    dz = pipeline(DEMO_FILE)
 
-print(*list(dz.itertracks(yield_label = True))[:10], sep=&quot;\n&quot;)
+    with open("diarization.txt", "w") as text_file:
+        text_file.write(str(dz))
+
 
 if __name__ == "__main__":
-    
-    from pydub import AudioSegment
-
-    t1 = 0 * 1000 # works in milliseconds
-    t2 = 20 * 60 * 1000
-
-    newAudio = AudioSegment.from_wav("download.wav")
-    a = newAudio[t1:t2]
-    a.export("audio.wav", format="wav") 
-
-
     main()
